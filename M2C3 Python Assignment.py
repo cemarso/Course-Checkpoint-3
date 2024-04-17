2ee47378929eb492e498ef2ed98181864246b793
cemarso <150005348+cemarso@users.noreply.github.com>
2024-04-17 20:28:45 +0200

    Add files via upload

---
+++ M2C3 Python Assignment.py
@@ -0,0 +1,60 @@
+#Exercise 1
+
+name = 'Cecilia'
+age = '45'
+fruits = ['apple','orange', 'lemon']
+verdadero = True
+
+#Exercise 2
+
+letters = 'abcdefg'
+print(letters[0:3])
+
+#Exercise 3
+
+fruits = ['apple','orange', 'lemon']
+print(fruits[0])
+
+#Exercise 4
+
+number = 45
+new_number = number + 10
+print(new_number)
+
+#Exercise 5
+
+fruits = ['apple','orange', 'lemon']
+print(fruits[-1])
+
+#Exercise 6 
+
+names = 'harry,alex,susie,jared,gail,conner'
+print(names.split(','))
+
+#Exercise 7 
+
+sentence = 'this is my string'
+first_word = sentence[0:4]
+first_word_upper = first_word.upper()
+rest = sentence[4:]
+new_string = first_word_upper + rest
+print(new_string)
+
+#Exercise 8
+
+number = 45
+sentence = f'I am {number} years old'
+print(sentence)
+
+#Exercise 9 
+
+greeting = 'hello World'
+print(greeting)
+
+#Exercise Hola
+
+frase = 'hola amigo'
+print(frase)
+query = frase.find('amigo')
+frase = frase.replace('hola', 'adiós')
+print(frase)
